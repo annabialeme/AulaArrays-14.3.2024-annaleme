85fec39cebf44abfcad3ff2636cd59ab89b252b0
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 20:41:47 -0300

    exemplo 2

---
+++ atividadearray6.1.js
@@ -0,0 +1,8 @@
+//começamos com seis numeros
+let numeros = [20, 30, 40, 50, 40, 50];
+
+// ele recebe um parâmetro que é o elemento que desejamos encontrar. Neste caso, queremos encontrar o último índice do número 40.
+let ultimoIndice = numeros.lastIndexOf(40);
+
+//é atribuído à variável ultimoIndice, que é então impressa no console.
+console.log(ultimoIndice);
