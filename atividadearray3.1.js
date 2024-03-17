6e625aa2e0ea6afb11d1e29bc333ed224018e8b2
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 20:37:05 -0300

    exemplo 2

---
+++ atividadearray3.1.js
@@ -0,0 +1,10 @@
+//começamos com cinco numeros
+let numeros = [10, 11, 12, 13, 14];
+
+//esse metodo recebe um parâmetro que é o elemento que desejamos verificar se está presente no array. Neste caso, queremos verificar se o número 13 está presente.
+let incluiTres = numeros.includes(13); 
+
+// esse metodo verifica se o elemento especificado está presente no array. Se estiver presente, ele retorna true; caso contrário, retorna false.
+
+//é atribuído à variável incluiTres
+console.log(incluiTres);
