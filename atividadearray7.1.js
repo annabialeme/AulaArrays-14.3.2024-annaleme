bca9c5ac04cc2611fc138c469de8f8a13ada2abe
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 20:47:15 -0300

    exemplo 2

---
+++ atividadearray7.1.js
@@ -0,0 +1,8 @@
+//começamos com cinco numeros
+let numeros = [30, 40, 50, 40, 60];
+
+//ele recebe um parâmetro que é o elemento que desejamos encontrar. Neste caso, queremos encontrar o primeiro índice do número 40.
+let indice = numeros.indexOf(40);
+
+// atribuído à variável primeiroIndice, que é então impressa no console.
+console.log(indice);
