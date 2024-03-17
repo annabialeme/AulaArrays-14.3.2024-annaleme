8b632b02f37307b470c335ba87b1216604bd1cfd
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 20:38:08 -0300

    exemplo 2

---
+++ atividadearray4.1.js
@@ -0,0 +1,8 @@
+//começa com cinco números
+let numeros = [20, 21, 22, 23, 24];
+
+//ele não aceita nenhum parâmetro e irá inverter a ordem dos elementos
+numeros.reverse();
+
+// o array numeros é modificado para conter os elementos na ordem inversa.
+console.log(numeros);
