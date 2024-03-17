3feeeb59560e0c82085935516206e28be0fbfaca
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 20:34:32 -0300

    exemplo 2

---
+++ atividadearray2.2.js
@@ -0,0 +1,8 @@
+//começa com quatro letras do alfabeto
+let letras = ['e', 'f', 'g', 'h'];
+
+let resultado = letras.join ('-');
+//especifica o separador a ser utilizado para unir os elementos do array
+
+console.log(resultado);
+//o resultado retornado pelo método join é atribuído à variável resultado, que é então impressa no console.
