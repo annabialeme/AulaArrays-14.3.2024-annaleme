bdcfbf426bab3769cbe3cf269edb0139dc99e4d2
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 20:39:31 -0300

    exemplo 2

---
+++ atividadearray5.1.js
@@ -0,0 +1,10 @@
+//começamos com cinco numeros
+let numeros = [3, 4, 6, 8, 10]; 
+
+//Aqui usamos uma função de seta que verifica se o número é par (numero % 2 === 0).
+let temPar = numeros.some(numero => numero % 2 === 0);
+
+//verifica se pelo menos um dos elementos do array satisfaz a condição especificada pela função de callback. Se pelo menos um elemento satisfazer a condição, ele retorna true; caso contrário, retorna false.
+
+//é atribuído à variável temPar, que é então impressa no console.
+console.log(temPar);
