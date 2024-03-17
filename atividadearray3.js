c47eca1092374c4ff43439592dae29a6706d69cd
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 19:38:41 -0300

    array includes

---
+++ atividadearray3.js
@@ -0,0 +1,10 @@
+//começamos com cinco numeros
+let numeros = [5, 6, 7, 8, 9];
+
+//esse metodo recebe um parâmetro que é o elemento que desejamos verificar se está presente no array. Neste caso, queremos verificar se o número 8 está presente.
+let incluiTres = numeros.includes(8); 
+
+// esse metodo verifica se o elemento especificado está presente no array. Se estiver presente, ele retorna true; caso contrário, retorna false.
+
+//é atribuído à variável incluiTres
+console.log(incluiTres);
