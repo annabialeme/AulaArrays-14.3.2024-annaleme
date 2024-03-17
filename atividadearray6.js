694bb138c07ea556a2f84105632df191cce07339
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 19:56:33 -0300

    array lastIndexOf

---
+++ atividadearray6.js
@@ -0,0 +1,8 @@
+//começamos com seis numeros
+let numeros = [10, 20, 30, 40, 30, 50];
+
+// Ele recebe um parâmetro que é o elemento que desejamos encontrar. Neste caso, queremos encontrar o último índice do número 30.
+
+
+let ultimoIndice = numeros.lastIndexOf(40);
+console.log(ultimoIndice);
