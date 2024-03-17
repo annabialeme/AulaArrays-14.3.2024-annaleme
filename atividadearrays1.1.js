3ede63e54de43d5275f29c2e12a043f92430fa27
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 20:33:25 -0300

    exemplo 2

---
+++ atividadearrays1.1.js
@@ -0,0 +1,8 @@
+let quadrados = numeros.map(numero => {
+  //a função da seta que recebe o parametro 'numero' 
+  
+  return numero * numero;
+   //dentro da função, cada numero é multiplicado por si mesmo e é elevado ao quadrado e retornado
+ });
+ console.log(quadrados);
+ //cada elemento do array original é substituido pelo resultado correspondente a função 
