c7ea3eba09844e7a13ab827b026328b1e9a740e7
annabialeme <annaleme.tds1.senai@gmail.com>
2024-03-17 19:17:27 -0300

    array map

---
+++ atividadearrays1.js
@@ -0,0 +1,11 @@
+//começa com cinco numeros
+let numeros = [10, 11, 12, 13, 14];
+
+let quadrados = numeros.map(numero => {
+ //a função da seta que recebe o parametro 'numero' 
+ 
+ return numero * numero;
+  //dentro da função, cada numero é multiplicado por si mesmo e é elevado ao quadrado e retornado
+});
+console.log(quadrados);
+//cada elemento do array original é substituido pelo resultado correspondente a função 
